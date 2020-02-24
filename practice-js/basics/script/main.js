e48d5c58ea700cbdd6d27df8d4a700c38c4ba5b2
Pablo Mira <pablo.mira@babel.es>
2020-02-24 09:47:01 +0100

    Add practice-js basics

---
+++ practice-js/basics/script/main.js
@@ -0,0 +1,29 @@
+let myImage = document.querySelector('img');
+let myHeading = document.querySelector('h1');
+let myButton = document.querySelector('button');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
