145e0a968d5a7be2f44ba54c4c7591101a9db711
Pablo Mira <pablo.mira@babel.es>
2020-02-28 10:33:58 +0100

    Add enter event for button

--- promise-prac/main.js
+++ promise-prac/main.js
@@ -3,10 +3,20 @@ const countInput = document.querySelector('input');
 
 countInput.addEventListener('input', function() {
     let inputValue = countInput.value;
-    
+
     if (inputValue) {
         countBtn.textContent = inputValue == 1 ? `Contar ${inputValue} segundo` : `Contar ${inputValue} segundos`;
     } else {
         countBtn.textContent = 'Contar';
     }
+});
+
+countInput.addEventListener('keyup', function(e) {
+    if (e.key == 'Enter') {
+        countBtn.click();
+    }
+});
+
+countBtn.addEventListener('click', function() {
+    console.log('Button pressed');
 });
