deadd2291f687710d17616ad315e19bb903f984f
Pablo Mira <pablo.mira@babel.es>
2020-02-28 10:45:58 +0100

    Add "add counter to list" feature

--- promise-prac/main.js
+++ promise-prac/main.js
@@ -1,7 +1,8 @@
 const countBtn = document.querySelector('button');
 const countInput = document.querySelector('input');
+const countList = document.querySelector('button + ul');
 
-countInput.addEventListener('input', function() {
+countInput.addEventListener('input', function () {
     let inputValue = countInput.value;
 
     if (inputValue) {
@@ -11,13 +12,20 @@ countInput.addEventListener('input', function() {
     }
 });
 
-countInput.addEventListener('keyup', function(e) {
+countInput.addEventListener('keyup', function (e) {
     if (e.key == 'Enter') {
         countBtn.click();
         this.select();
     }
 });
 
-countBtn.addEventListener('click', function() {
-    console.log('Button pressed');
+countBtn.addEventListener('click', function () {
+    let inputValue = countInput.value;
+
+    if (inputValue) {
+        let li = document.createElement('li');
+        // TODO Add counter and remove 0 value
+        li.textContent = `0/${inputValue}`;
+        countList.appendChild(li);
+    }
 });
