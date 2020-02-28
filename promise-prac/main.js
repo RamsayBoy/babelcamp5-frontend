74192e44f435d116933cf57feef64bdcbf87dd5a
Pablo Mira <pablo.mira@babel.es>
2020-02-28 10:19:00 +0100

    Add dynamic button text change

---
+++ promise-prac/main.js
@@ -0,0 +1,12 @@
+const countBtn = document.querySelector('button');
+const countInput = document.querySelector('input');
+
+countInput.addEventListener('input', function() {
+    let inputValue = countInput.value;
+    
+    if (inputValue) {
+        countBtn.textContent = inputValue == 1 ? `Contar ${inputValue} segundo` : `Contar ${inputValue} segundos`;
+    } else {
+        countBtn.textContent = 'Contar';
+    }
+});
