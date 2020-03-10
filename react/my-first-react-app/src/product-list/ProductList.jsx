0fb93f8791735aec29e2dc808f0bf64f10bd43fe
Pablo Mira <pablo.mira@babel.es>
2020-03-10 11:37:12 +0100

    Add view change

--- react/my-first-react-app/src/product-list/ProductList.jsx
+++ react/my-first-react-app/src/product-list/ProductList.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import './ProductList.css'
 
-function ProductList(props) {
-    const productList = props.products.map(product => {
-        return (
-            <li key={product.id}>
-                <h2>{product.name}</h2>
-                <img src={product.image} alt={product.name} />
-                <p>{product.description.length > 150 ? product.description.slice(0, 150) + '...' : product.description}</p>
-                <div className="price">{product.price} €</div>
-                <div>
-                    <button className="button">add to cart</button>
-                </div>
-            </li>
-        );
-    });
+class ProductList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gridView: false,
+        }
+    }
 
-    return (
-        <div className="wrapper">
-            <h1>Products</h1>
-            <div className="views">
-                <i className="material-icons">
-                    view_list
-               </i>
-                <i className="material-icons">
-                    view_module
-              </i>
+    render() {
+        const productList = this.props.products.map(product => {
+            return (
+                <li key={product.id}>
+                    <h2>{product.name}</h2>
+                    <img src={product.image} alt={product.name} />
+                    <p>{product.description.length > 150 ? product.description.slice(0, 150) + '...' : product.description}</p>
+                    <div className="price">{product.price} €</div>
+                    <div>
+                        <button className="button">add to cart</button>
+                    </div>
+                </li>
+            );
+        });
+
+        return (
+            <div className="wrapper">
+                <h1>Products</h1>
+                <div className="views">
+                    <i onClick={() => this.setState( { gridView: false } )} className="material-icons">
+                        view_list
+                   </i>
+                    <i onClick={() => this.setState( { gridView: true } )} className="material-icons">
+                        view_module
+                  </i>
+                </div>
+                <ul className={ this.state.gridView ? 'grid' : '' }>
+                    {productList}
+                </ul>
             </div>
-            <ul>
-                {productList}
-            </ul>
-        </div>
-    );
+        );
+    }
 }
 
 export default ProductList;
